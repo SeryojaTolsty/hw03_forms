d4d7f78ab534089fe8af1857ac3bce6ceb742e11
Sergei Anisimov <seroja.92@mail.ru>
2023-01-10 22:24:58 +0300

    пасхалка (сродни задачам в яндехе)

--- yatube/about/urls.py
+++ yatube/about/urls.py
@@ -8,4 +8,5 @@ app_name = 'about'
 urlpatterns = [
     path('author/', views.AboutAuthorView.as_view(), name='author'),
     path('tech/', views.AboutTechView.as_view(), name='tech'),
+    path('eastereggs/', views.EasterEggs.as_view(), name='eastereggs'),
 ]
--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.base import TemplateView
 
+# Create your views here.
+
 
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
@@ -9,3 +11,5 @@ class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
 
 
+class EasterEggs(TemplateView):
+    template_name = 'about/eastereggs.html'
